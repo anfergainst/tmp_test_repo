638cc42ae048b9dfed695428c0732807870614e9
Anderson Ayres <anfergainst@gmail.com>
2024-01-05 09:46:50 +0000

    check if post exist to create or update wp function

--- api/wp-function/functions.php
+++ api/wp-function/functions.php
@@ -1,3 +1,8 @@
+/**
+ * DevOps.click CUSTOM API for WordPress.
+ * Clone/Update Real State Posts
+ */
+
 add_action( 'rest_api_init', function () {
     register_rest_route( 'devops/v1', '/create-from-template/', array(
         'methods' => 'POST',
@@ -7,6 +12,23 @@ add_action( 'rest_api_init', function () {
 //         }
 	) );
 } );
+
+function get_post_by_title($title) {
+    $args = array(
+        'post_type'  => 'post', // or your custom post type if applicable
+        'post_status' => 'any',
+        'title' => $title,
+        'posts_per_page' => 1,
+    );
+
+    $posts = get_posts($args);
+
+    if ( $posts ) {
+        return $posts[0]->ID;
+    }
+
+    return false;
+}
 
 function create_from_template( $request ) {
     $post_data = $request->get_json_params();
@@ -43,6 +65,30 @@ function create_from_template( $request ) {
         // More fields here...
         'post_status'   => 'publish',
     );
+
+    // Check if a post with the same title already exists
+    $existing_post_id = get_post_by_title($post_data['title']);
+
+    if ( $existing_post_id ) {
+        // Post exists, update it
+        $updated_post_data = array(
+            'ID'           => $existing_post_id,
+            // Add other fields you want to update
+            'post_content' => $new_content, // Assume $new_content is prepared
+            // ...
+        );
+
+        $updated_post_id = wp_update_post( $updated_post_data );
+
+        if ( is_wp_error( $updated_post_id ) ) {
+            return new WP_Error( 'post_update_failed', 'Failed to update post', array( 'status' => 500 ) );
+        }
+
+        return array( 'message' => 'Post updated successfully!', 'post_id' => $updated_post_id );
+    } else {
+        // Post does not exist, create it
+        // ... [existing code to create a new post]
+    }
 
     // Insert the new post
     $new_post_id = wp_insert_post( $new_post_data );
